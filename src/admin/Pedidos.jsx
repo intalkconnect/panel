69da09a890e8fea150c1e287e9d7e4c0ed5b0e4b
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-04-26 20:30:35 -0300

    Update Pedidos.jsx

--- src/admin/Pedidos.jsx
+++ src/admin/Pedidos.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { supabase } from "../data/supabaseClient";
 
-const KanbanPedidos = () => {
+const Pedidos = () => {
   const [pedidos, setPedidos] = useState([]);
   const [novosPedidos, setNovosPedidos] = useState([]);
   const [alertaNovoPedido, setAlertaNovoPedido] = useState(false);
