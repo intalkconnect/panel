43eaa1cb1251b5167d13f849b09f8976d9e818e2
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-04-25 23:10:51 -0300

    Update and rename TotemNaoConfigurado.jsx to padrao.jsx

---
+++ src/components/telas/padrao.jsx
@@ -0,0 +1,16 @@
+// src/components/TotemNaoConfigurado.jsx
+import React from "react";
+
+const TotemNaoConfigurado = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-red-600">
+      <img
+        src="https://onyedkfjdkplbaxpetln.supabase.co/storage/v1/object/public/zapediu//zapediuBG.png"
+        alt="Zapediu Background"
+        className="max-w-full max-h-full object-contain"
+      />
+    </div>
+  );
+};
+
+export default padrao;
