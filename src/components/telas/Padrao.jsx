bdf8fb93e0a2de043143899a2db15680da7cff3a
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-04-25 23:21:52 -0300

    Update Padrao.jsx

--- src/components/telas/Padrao.jsx
+++ src/components/telas/Padrao.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Padrao = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-red-600">
+    <div className="min-h-screen flex items-center justify-center" style={{ backgroundColor: '#F74036' }}>
       <img
         src="https://onyedkfjdkplbaxpetln.supabase.co/storage/v1/object/public/zapediu//zapediuBG.png"
         alt="Zapediu Background"
