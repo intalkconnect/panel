de7d2ee57ffb4924d725e519c0af5479165ce639
intalkconnect <122463183+intalkconnect@users.noreply.github.com>
2025-05-04 20:31:41 -0300

    Update Pdv.jsx

--- src/admin/Pdv.jsx
+++ src/admin/Pdv.jsx
@@ -156,7 +156,7 @@ const Pdv = () => {
     : produtos;
 
   return (
-       <div className="flex h-[95vh] max-h-[95vh] bg-white text-gray-800 border border-gray-300 rounded-xl shadow-xl overflow-hidden">
+       <div className="flex h-[95vh] max-h-[95vh] bg-white text-gray-800 border border-gray-300 rounded-xl shadow-xl overflow-hidden ml-[3px] mr-[3px] w-full">
       {/* Carrinho */}
       <aside className="w-1/3 bg-gray-100 border-r p-4 flex flex-col justify-between">
         <div>
